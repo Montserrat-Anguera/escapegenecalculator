51ffcc0e12d79b6cadef2c0002868343fb3a09bd
Harrison Wang <harrisonized@gmail.com>
2023-01-16 21:01:28 -0500

    standardize line spacings

--- R/calculate_sequence_lengths.R
+++ R/calculate_sequence_lengths.R
@@ -1,10 +1,8 @@
-# Directly calculate sequence lengths
-# Takes 4-5 minutes to run on Ubuntu 20.04 with 32G RAM.
-
+## Directly calculate sequence lengths
+## Takes 4-5 minutes to run on Ubuntu 20.04 with 32G RAM.
 
 library(logr)
 library(seqinr)
-
 
 
 # Input parameters
@@ -14,23 +12,26 @@ out_dir = file.path(in_dir, "length")
 output_filename = 'Mus_musculus_casteij_lengths.tsv'
 
 
-
 # Start Log
 log <- log_open(paste("calculate_sequence_lengths ", Sys.time(), '.log', sep=''))
 log_print(paste('input file: ', file.path(in_dir, input_filename)))
 log_print(paste('output file: ', file.path(out_dir, output_filename)))
 
 
+# ----------------------------------------------------------------------
+# Read Data
 
 # Read fasta file
 start_time = Sys.time()
 log_print(paste('reading fasta file...', start_time))
 fasta_file <- read.fasta(file.path(in_dir, input_filename))
 end_time = Sys.time()
-log_print(paste('reading fasta file...', end_time))
+log_print(paste('opened.', end_time))
 log_print(paste('time to open: ', end_time - start_time))
 
 
+# ----------------------------------------------------------------------
+# Processing
 
 # Convert fasta file to dataframe
 log_print(paste('converting to dataframe...', Sys.time()))
@@ -39,6 +40,8 @@ names(fasta_length) <- names(fasta_file)
 fasta_length <- data.frame(fasta_length)
 
 
+# ----------------------------------------------------------------------
+# Save Output
 
 # Save output
 log_print(paste('writing file...', Sys.time()))
